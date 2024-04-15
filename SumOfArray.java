6cf1db0f2c22c6126c23d9d21f7b593b4b656376
Anusha Sahu <96466374+Anusha9300@users.noreply.github.com>
2024-04-15 11:07:28 +0530

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SumOfArray {
+    public static void main(String[] args) {
+        int arr[] = new int[3];
+        
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+        int sum =0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>0){
+            sum+=arr[i];
+            }
+        }
+        System.out.println("Sum of possitive numbers in array "+sum);
+    }
+}
